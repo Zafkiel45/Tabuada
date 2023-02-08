28c765ef06c953d0773b6beb1d3112bb2d6df24c
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2023-02-08 11:28:09 -0300

    up

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,9 +2,14 @@ function clicou() {
     let num = document.getElementById('num');
     let num_1 = Number(num.value);
     let res = document.getElementById('res');
-
     if(num_1 == 0 || num.value.lenght == 0 ) {
         alert('erro')
+    } else {
+        res.innerHTML = ``
+        for(let i = 0; i <= 10; i++) {
+            let p = document.createElement('p');
+            p.innerHTML = `${i} x ${num_1} = ${i * num_1}`
+            res.appendChild(p)
+        }
     }
-    console.log(num_1)
 }
